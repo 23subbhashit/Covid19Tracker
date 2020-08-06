52c6e88a2a43f00844731ffa8dae98e518274ad5
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-08-06 20:29:23 +0530

    third commit

--- src/components/Footer/Address.vue
+++ src/components/Footer/Address.vue
@@ -1,14 +1,53 @@
 <template>
-  <div class="col-7 col-sm-5">
-    <h5>Our Address</h5>
-      <address>
-          121, Clear Water Bay Road<br>
-          Clear Water Bay, Kowloon<br>
-          HONG KONG<br>
-          <i class="fa fa-phone fa-lg"></i>: +852 1234 5678<br>
-          <i class="fa fa-fax fa-lg"></i>: +852 8765 4321<br>
-          <i class="fa fa-envelope fa-lg"></i>: 
-          <a href="mailto:confusion@food.net">confusion@food.net</a>
-      </address>
-    </div>
+  <div clas="">
+  <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 offset-1 col-sm-2">
+                    <h5>Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="./index.html"><i class="fa fa-home"></i> Home</a></li>
+                        <li><a href="./aboutus.html"><i class="fa fa-info"></i> About</a></li>
+                        <li><a href="#"><i class="fa fa-list"></i> Menu</a></li>
+                        <li><a href="./contactus.html"><i class="fa fa-address-card"></i> Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-7 col-sm-5">
+                    <h5>Our Address</h5>
+                    <address>
+                        Doddaballapur Main Road, Avalahalli<br>
+                        Yelahanka, Bengaluru, Karnataka 560064<br>
+                        India<br>
+                        <i class="fa fa-phone fa-lg"></i>: +91 9140927375 <br>
+                        <a href="https://23subbhashit.github.io/">My portfolio</a>
+                    </address>
+                </div>
+                <div class="col-12 col-sm-4 align-self-center">
+                    <div class="text-center">
+                            <div class="text-center">
+                        <a href="https://www.facebook.com/subbhashit.mukherjee" class="btn btn-facebook"><i class="fa fa-facebook fa-lg"></i></a>
+                        <a href="https://www.linkedin.com/in/subbhashit-mukherjee-71849a148/" class="btn btn-linkedin"><i class="fa fa-linkedin fa-lg"></i></a>
+                        <a href="https://twitter.com/SMukhkherjee" class="btn btn-twitter"><i class="fa fa-twitter fa-lg"></i></a>
+                        <!-- <a href="http://youtube.com/" class="btn btn-google"><i class="fa fa-youtube fa-lg"></i></a> -->
+                        <a href="subbhashitmukherjee@gmail.com" class="btn btn-google"><i class="fa fa-envelope-o fa-lg"></i></a>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-auto">
+                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+  </div>
 </template>
+<style scoped>
+.footer {
+  background-color: #B993D6;
+  margin: 0px auto;
+  padding: 20px 0px;
+}
+</style>
