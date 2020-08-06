5c9490bd344b48570854a386f3f46bd108387d5c
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-08-06 20:16:18 +0530

    third commit

--- src/components/Footer/Address.vue
+++ src/components/Footer/Address.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-  <footer class="footer">
-    <div class="col-7 col-sm-5">
-        <h5>My Address</h5>
-        <address>
-        Doddaballapur Main Road, Avalahalli <br>
-		Yelahanka, Bengaluru, Karnataka 560064<br>
-		India<br>
-        <i class="fa fa-phone"></i>: +91 9140927375<br>
-        <i class="fa fa-envelope fa-lg"></i>: 
-        <a href="subbhashitmukherjee@gmail.com">subbhashitmukherjee@gmail.com</a>
-		</address>
+  <div class="col-7 col-sm-5">
+    <h5>Our Address</h5>
+      <address>
+          121, Clear Water Bay Road<br>
+          Clear Water Bay, Kowloon<br>
+          HONG KONG<br>
+          <i class="fa fa-phone fa-lg"></i>: +852 1234 5678<br>
+          <i class="fa fa-fax fa-lg"></i>: +852 8765 4321<br>
+          <i class="fa fa-envelope fa-lg"></i>: 
+          <a href="mailto:confusion@food.net">confusion@food.net</a>
+      </address>
     </div>
-  </footer>
-  </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 Vue.config.productionTip = false
 
 new Vue({
