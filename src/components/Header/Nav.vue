5c0a5a43ce1254cd1b06e83a44273c6995c2d87e
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-08-07 12:54:48 +0530

    third commit

---
+++ src/components/Header/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+   <nav class="navbar custom-nav fixed-top navbar-expand-lg navbar-light bg-light">
+     <div class="container">
+        <router-link class="navbar-brand" to="/">Vue Shop</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/products" class="nav-link" href="#">Products</router-link>
+            </li>
+
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link" href="#">About</router-link>
+            </li>
+          
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <a class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#login">Get Start</a>
+            <a class="btn btn-outline-info border-0 mx-2 my-2 my-sm-0" data-toggle="modal" data-target="#miniCart">
+              <i class="fas fa-cart-plus"></i>
+            </a>
+          </form>
+        </div>
+
+     </div>
+
+</nav>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    msg: String
+  },
+  components:{}
+};
+</script>
+
