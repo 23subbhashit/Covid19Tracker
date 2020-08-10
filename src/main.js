ce5bfca223bd2a1b619a3da0fd1a8f10fbf30751
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-08-10 19:34:44 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
