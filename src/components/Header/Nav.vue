b0b20fb8fee92a73cc6185f9248817058290bf99
subbhashit mukherjee <subbhashitmukherjee@gmail.com>
2020-08-11 13:14:34 +0530

    first commit

--- src/components/Header/Nav.vue
+++ src/components/Header/Nav.vue
@@ -24,8 +24,6 @@
           <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <a class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#login">Get Start</a>
-            <a class="btn btn-outline-info border-0 mx-2 my-2 my-sm-0" data-toggle="modal" data-target="#miniCart">
-            </a>
           </form>
         </div>
 
